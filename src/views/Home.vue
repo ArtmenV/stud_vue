79fcd36d9acc08a951e33b24a937a3dedc7e8081
Artmen_V <viliutich.artem@gmail.com>
2019-10-11 10:39:39 +0700

    hello

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Hello {{name}}!</h1>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  name: "home",
+  data() {
+    return {
+      name: "Viliutich Artem"
+    };
   }
-}
+};
 </script>
