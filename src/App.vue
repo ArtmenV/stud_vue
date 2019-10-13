6d9909bea4ba7845077e1ac88fc6219b0857dc55
Artmen_V <viliutich.artem@gmail.com>
2019-10-13 14:05:01 +0700

    add_list

--- src/App.vue
+++ src/App.vue
@@ -15,10 +15,20 @@
             <router-link class="head__link white--text" :to="`${link.url}`">{{link.title}}</router-link>
           </v-btn>
         </v-toolbar-items>
-        <v-navigation-drawer app class="indigo" v-model="drawer">
-          <p>test</p>
-        </v-navigation-drawer>
       </v-toolbar>
+
+      <v-navigation-drawer app v-model="drawer" class="indigo">
+        <v-list>
+          <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
+            <v-list-tile-action>
+              <v-icon class="white--text">{{ link.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title class="white--text">{{ link.text }}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
     </header>
 
     <router-view></router-view>
@@ -34,6 +44,11 @@ export default {
         { title: "Home", url: "/" },
         { title: "Login", url: "/login" },
         { title: "Registration", url: "/registration" }
+      ],
+      links: [
+        { icon: "dashboard", text: "Dashboard", route: "/" },
+        { icon: "folder", text: "My Project", route: "/projects" },
+        { icon: "person", text: "Team", route: "/team" }
       ]
     };
   }
